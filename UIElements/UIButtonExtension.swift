5d3d26402adb7f62c39ccbd06b138036465e0716
Pimpaporn Chaichompoo <you@example.com>
2017-05-04 15:18:46 +0700

    added button loading element

--- UIElements/UIButtonExtension.swift
+++ UIElements/UIButtonExtension.swift
@@ -10,26 +10,17 @@ import UIKit
 
 @IBDesignable
 class UIButtonExtension: UIButton {
-
-    @IBInspectable var bgAlpha: CGFloat? = 0.4{ didSet { setBackgroundAlpha() }}
-    @IBInspectable var color: UIColor? = UIColor.white{ didSet { setBackgroundAlpha() }}
-    @IBInspectable var colorBorder: UIColor? = UIColor.red{ didSet { setBackgroundAlpha() }}
+    
+    @IBInspectable var colorBorder: UIColor? = UIColor.clear{ didSet { setBorder() }}
     @IBInspectable var borderWidth: CGFloat? = 1{ didSet { setBorder() }}
-
-    func setBackgroundAlpha() {
-        
-        self.backgroundColor = color?.withAlphaComponent(bgAlpha!)
-    }
     
     func setBorder(){
-        
         self.layer.borderWidth = borderWidth!
         self.layer.borderColor = colorBorder?.cgColor
     }
     
     override func layoutIfNeeded() {
         super.layoutSubviews()
-        setBackgroundAlpha()
         setBorder()
     }
     
@@ -42,5 +33,31 @@ class UIButtonExtension: UIButton {
             layer.masksToBounds = newValue > 0
         }
     }
+}
+
+extension UIButton{
     
+    func loadingIndicator(show: Bool) {
+        let tag = 9876
+        if show {
+            self.alpha = 0.6
+            self.isUserInteractionEnabled = false
+            self.tintColor = UIColor.init(red: 255.0/255.0, green: 255.0/255.0, blue: 255.0/255.0, alpha: 0.6)
+            //setTitleColor(UIColorFromRGB("F21B3F"), forState: .Normal)
+            let indicator = UIActivityIndicatorView()
+            let buttonHeight = self.bounds.size.height
+            let buttonWidth = self.bounds.size.width
+            indicator.center = CGPoint(x:buttonWidth - 30, y:buttonHeight/2)
+            indicator.tag = tag
+            self.addSubview(indicator)
+            indicator.startAnimating()
+        } else {
+            self.alpha = 1
+            self.isUserInteractionEnabled = true
+            if let indicator = self.viewWithTag(tag) as? UIActivityIndicatorView {
+                indicator.stopAnimating()
+                indicator.removeFromSuperview()
+            }
+        }
+    }
 }
--- UIElements/ViewController.swift
+++ UIElements/ViewController.swift
@@ -10,8 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var btnTest: UIButtonExtension!
+    var showactivity:Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let baseFunction:BaseFunction = BaseFunction()
+        print("check internet connection %@",baseFunction.isInternetAvailable())
+        
+    }
+    
+    @IBAction func btnTestPressed(_ sender: Any) {
+        btnTest.loadingIndicator(show: true)
+        self.perform(#selector(resetLoading), with: nil, afterDelay: 2)
+    }
+    
+    func resetLoading(){
+        btnTest.loadingIndicator(show: false)
     }
 }
 
